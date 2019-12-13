3dec80737c6457028073649155b95600b195ffb4
Sylver11 <justus@cognician.com>
2019-12-13 13:46:34 +0200

    with for loop and fake db call

---
+++ src/ring_server/db/queries.clj
@@ -0,0 +1,9 @@
+(ns ring-server.db.queries)
+
+(def projects-listed
+  {1 {:title "Project 1"
+      :description "This is the first project"}
+   2 {:title "Project 2"
+      :description "Wuuuhuu this is our second Project"}
+   3 {:title "Project 3"
+      :description "Project 3 has arrived"}})
--- src/ring_server/views/projects.clj
+++ src/ring_server/views/projects.clj
@@ -1,14 +1,20 @@
 (ns ring-server.views.projects
   (:require
    [hiccup.core]
-   [hiccup.page :as page]))
+   [hiccup.page :as page]
+   [ring-server.db.queries.projects-listed :as projects-listed]))
 
+
+
+(def projects-listed)
 
 (defn list-projects [request]
   (page/html5
-   [:ul [:li [:a {:href "/project/1"} "Project 1"]]
-    [:li [:a {:href "/project/2"} "Project 2"]]
-    [:li [:a {:href "/project/3"} "Project 3"]]]))
+   [:ul
+    (for [[id {:keys [title]}] projects-listed]
+      [:li
+       [:a [:href (str "project/" id)]
+        title]])]))
 
 (defn projects-id [id]
   (page/html5
