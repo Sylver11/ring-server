12780c6ac2862cb1303054ec8bc797fdfe0e26d1
Sylver11 <justus@cognician.com>
2019-12-17 09:12:50 +0200

    including start stop atom

--- src/ring_server/core.clj
+++ src/ring_server/core.clj
@@ -1,12 +1,10 @@
 (ns ring-server.core
-
   (:require [ring.adapter.jetty :as jetty]))
 
 ;;defer to funtions for each page handler
 ;;put those handlers in their own namespace
 ;;use hiccup to render the page body
 ;;explore templating -common header/footer,unique content per page
-
 
 (defn handler [request]
   (cond (= (:uri request) "/home")
@@ -34,7 +32,6 @@
   (when-some [server @*server]
     (.stop server))
   (reset! *server nil))
-
 
 (defn reset-server! []
   (stop-server!)
